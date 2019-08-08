0f95f32954a57aa2dfbe3873f161929071f680aa
Colin Holzman <colin.holzman@protonmail.com>
2019-08-07 21:23:09 -0400

    add instructions for using gallery plugin

--- _plugins/gallery.rb
+++ _plugins/gallery.rb
@@ -1,10 +1,21 @@
 
-#add <source>/gallery folder with images
-#add gallery/ to exclude list in _config.yml
+#install ImageMagick
+#add gem "mini_magick" to your site's Gemfile
+#run gem install mini_magick
+#add <site source>/gallery folder with images
+#add gallery/ to exclude list in your site's _config.yml
+#run jekyll build
+
+require 'mini_magick'
+
 module GalleryGenerator
     class GalleryGenerator < Jekyll::Generator
         def generate(site)
-            print File.directory?(File.join(site.source, "gallery"))
+            if File.directory?(File.join(site.source, "gallery"))
+            image = MiniMagick::Image.open(
+                "https://images.unsplash.com/photo-1516295615676-7ae4303c1c63"
+            )
+            print image
         end
     end
 end
